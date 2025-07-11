f649b41fcd5820fba4f6e3894393eb265e1a5a8a
gigwave24 <gigwave24@gmail.com>
2025-07-11 20:33:33 +0800

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -634,7 +634,7 @@
             <div class="chat-messages"></div>
             <div class="chat-controls">
                 <textarea class="chat-textarea" placeholder="Type your message here..." rows="1"></textarea>
-                <button class="chat-submit">
+                <button class="chat-submit" title="Send message">➤</button>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M22 2L11 13"></path>
                         <path d="M22 2l-7 20-4-9-9-4 20-7z"></path>
